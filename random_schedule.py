99527923ffb765f8a171f001553df38ce66d701b
Kenzo <kenzosakiyama@gmail.com>
2022-06-15 11:02:01 -0300

    Eliminando a dependencia de numpy

--- random_schedule.py
+++ random_schedule.py
@@ -1,5 +1,5 @@
 import os
-import numpy as np
+from random import choice
 from playsound import playsound
 import schedule
 
@@ -8,7 +8,7 @@ SOUNDS_PATH = "/home/kenzo/repos/MedoDelirioBrasilia/MedoDelirioBrasilia/MedoDel
 def play_random_sound() -> None:
 
     files = os.listdir(SOUNDS_PATH)
-    random_sound = np.random.choice(files)
+    random_sound = choice(files)
     print(f"- Reproduzindo: {random_sound}")
 
     playsound(SOUNDS_PATH + random_sound)
